852a385ee6979b53411144b1641671dc636030fe
Aaron Donaldson <atreids@outlook.com>
2022-05-31 22:07:41 +0100

    end exercise 1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,59 +1,37 @@
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>;
+import { useState } from "react";
+
+const Button = ({ onClick, name }) => {
+  return <button onClick={onClick}>{name}</button>;
 };
 
-const Part = (props) => {
+const Display = ({ count, name }) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {name}: {count}
     </p>
   );
 };
 
-const Content = (props) => {
-  console.log("Content called");
-  return (
-    <>
-      {props.parts.map((part, index) => (
-        <Part key={index} part={part.name} exercises={part.exercises} />
-      ))}
-    </>
-  );
-};
-
-const Total = (props) => {
-  let exercisesSum = 0;
-  props.parts.forEach((element) => {
-    exercisesSum += element.exercises;
-  });
-
-  return <p>Number of exercises: {exercisesSum}</p>;
-};
-
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGood = () => setGood(good + 1);
+  const increaseNeutral = () => setNeutral(neutral + 1);
+  const increaseBad = () => setBad(bad + 1);
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Give Feedback</h1>
+      <Button onClick={increaseGood} name="Good" />
+      <Button onClick={increaseNeutral} name="Neutral" />
+      <Button onClick={increaseBad} name="Bad" />
+      <h1>Statistics</h1>
+      <Display count={good} name="Good" />
+      <Display count={neutral} name="Neutral" />
+      <Display count={bad} name="Bad" />
     </div>
   );
 };
