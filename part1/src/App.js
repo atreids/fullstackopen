ec88bba63fcec8f4b65e2050e31f54eaf855c362
Aaron Donaldson <56371059+atreids@users.noreply.github.com>
2022-05-31 22:51:46 +0100

    exercise 1.7

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const Button = ({ onClick, name }) => {
   return <button onClick={onClick}>{name}</button>;
@@ -17,21 +17,37 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [averageScore, setAverageScore] = useState(0);
+  const isMounted = useRef(false);
 
-  const increaseGood = () => setGood(good + 1);
-  const increaseNeutral = () => setNeutral(neutral + 1);
-  const increaseBad = () => setBad(bad + 1);
+  useEffect(() => {
+    if (isMounted.current) {
+      setTotal(total + 1);
+      setAverageScore((good / (total + 1)) * 100);
+    } else {
+      isMounted.current = true;
+    }
+  }, [good, neutral, bad]);
+
+  const incrementer = (rating) => {
+    rating === "bad" ? setBad(bad + 1) : setBad(bad);
+    rating === "good" ? setGood(good + 1) : setGood(good);
+    rating === "neutral" ? setNeutral(neutral + 1) : setNeutral(neutral);
+  };
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button onClick={increaseGood} name="Good" />
-      <Button onClick={increaseNeutral} name="Neutral" />
-      <Button onClick={increaseBad} name="Bad" />
+      <Button onClick={() => incrementer("good")} name="Good" />
+      <Button onClick={() => incrementer("neutral")} name="Neutral" />
+      <Button onClick={() => incrementer("bad")} name="Bad" />
       <h1>Statistics</h1>
       <Display count={good} name="Good" />
       <Display count={neutral} name="Neutral" />
       <Display count={bad} name="Bad" />
+      <Display count={total} name="Total" />
+      <Display count={averageScore} name="Average Score" />
     </div>
   );
 };
