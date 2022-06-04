2902af49796b432cbb27550dbba8ba63037375d9
Aaron Donaldson <aaron-donaldson@github.com>
2022-06-04 18:11:48 +0100

    exercise 2.9 complete, cap sensitive tho

--- part2/exercises2/src/App.js
+++ part2/exercises2/src/App.js
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import areTheseObjectsEqual from "./components/AreTheseObjectsEqual";
 
+const PersonsList = ({ persons }) => {
+  return (
+    <>
+      <h2>Ledger:</h2>
+      {persons.map((person) => (
+        <li key={person.number}>
+          {person.name} {person.number}
+        </li>
+      ))}
+    </>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: "Arto Hellas",
-      number: "07872640044",
-    },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [personsToShow, setPersonsToShow] = useState(persons);
+  const [filter, setFilter] = useState("");
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
@@ -17,6 +33,12 @@ const App = () => {
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    const newFilter = event.target.value;
+    setFilter(newFilter);
+    updateList(newFilter);
   };
 
   const savePerson = (event) => {
@@ -29,6 +51,7 @@ const App = () => {
     if (personExist(newPerson)) {
       alert(newName + " already exists");
       setNewName("");
+      setNewNumber("");
       return;
     }
 
@@ -46,9 +69,31 @@ const App = () => {
     return false;
   };
 
+  const updateList = (filter) => {
+    if (filter === "") {
+      setPersonsToShow(persons);
+      return;
+    }
+    const initialValue = [];
+
+    setPersonsToShow(
+      persons.reduce((filteredPersons, currentPerson) => {
+        if (currentPerson.name.includes(filter)) {
+          filteredPersons.push(currentPerson);
+          return filteredPersons;
+        } else {
+          return filteredPersons;
+        }
+      }, initialValue)
+    );
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter: <input value={filter} onChange={handleFilterChange} />
+      </div>
       <form onSubmit={savePerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -60,12 +105,7 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
-        <li key={person.number}>
-          {person.name} {person.number}
-        </li>
-      ))}
+      <PersonsList persons={personsToShow} />
     </div>
   );
 };
