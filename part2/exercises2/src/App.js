fcf0bd589bbfb7c587371eba4287fad757876477
Aaron Donaldson <aaron-donaldson@github.com>
2022-06-04 16:20:40 +0100

    finished 2.7

--- part2/exercises2/src/App.js
+++ part2/exercises2/src/App.js
@@ -1,68 +1,73 @@
-import Course from "./components/Course";
+import { useState } from "react";
+import areTheseObjectsEqual from "./components/AreTheseObjectsEqual";
 
 const App = () => {
-  const courses = [
+  const [persons, setPersons] = useState([
     {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
+      name: "Arto Hellas",
+      number: "07872640044",
     },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  return <Course courses={courses} />;
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const savePerson = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+    };
+
+    if (personExist(newPerson)) {
+      alert(newName + " already exists");
+      setNewName("");
+      return;
+    }
+
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const personExist = (newPerson) => {
+    for (const person of persons) {
+      if (areTheseObjectsEqual(newPerson, person)) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={savePerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <li key={person.number}>
+          {person.name} {person.number}
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default App;
