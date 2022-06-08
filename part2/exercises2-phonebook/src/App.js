7996d26f63d90c52ffef06f77b70c15d84f23ba0
Aaron Donaldson <atreids@outlook.com>
2022-06-08 15:39:37 +0100

    finished exercises 2.19-2.20, part 2 complete

--- part2/exercises2-phonebook/src/App.js
+++ part2/exercises2-phonebook/src/App.js
@@ -4,12 +4,28 @@ import EntryForm from "./components/EntryForm";
 import Filter from "./components/Filter";
 import personServices from "./services/persons";
 
+const ErrorNotification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+  return <div className="error">{message}</div>;
+};
+
+const SuccessNotification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+  return <div className="success">{message}</div>;
+};
+
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [personsToShow, setPersonsToShow] = useState(persons);
   const [filter, setFilter] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
 
   useEffect(() => {
     personServices.getAll().then((initialPhonebook) => {
@@ -48,20 +64,38 @@ const App = () => {
             `${newPerson.name} already exists, replace the old number with a new number?`
           )
         ) {
-          personServices.update(person.id, newPerson).then((updatedPerson) => {
-            //updates displayed list by replacing the exisitng list with a new one where
-            //only the updated record is replaced.
-            setPersonsToShow(
-              personsToShow.map((person) =>
-                person.id === updatedPerson.id ? updatedPerson : person
-              )
-            );
-            setPersons(
-              persons.map((person) =>
-                person.id === updatedPerson.id ? updatedPerson : person
-              )
-            );
-          });
+          personServices
+            .update(person.id, newPerson)
+            .then((updatedPerson) => {
+              //updates displayed list by replacing the exisitng list with a new one where
+              //only the updated record is replaced.
+              setPersonsToShow(
+                personsToShow.map((person) =>
+                  person.id === updatedPerson.id ? updatedPerson : person
+                )
+              );
+              setPersons(
+                persons.map((person) =>
+                  person.id === updatedPerson.id ? updatedPerson : person
+                )
+              );
+              setSuccessMessage(`Successfully updated ${updatedPerson.name}`);
+              setTimeout(() => {
+                setSuccessMessage(null);
+              }, 5000);
+            })
+            .catch((error) => {
+              setErrorMessage(
+                `An error occured: ${person.name}'s information has already been removed from the server.`
+              );
+              personServices.getAll().then((serverPhonebook) => {
+                setPersons(serverPhonebook);
+                setPersonsToShow(serverPhonebook);
+              });
+              setTimeout(() => {
+                setErrorMessage(null);
+              }, 5000);
+            });
         }
         setNewName("");
         setNewNumber("");
@@ -76,6 +110,10 @@ const App = () => {
       setFilter("");
       setNewName("");
       setNewNumber("");
+      setSuccessMessage(`Successfully added ${returnedPerson.name}`);
+      setTimeout(() => {
+        setSuccessMessage(null);
+      }, 5000);
     });
   };
 
@@ -100,18 +138,30 @@ const App = () => {
 
   const handleDelete = (id, name) => {
     if (window.confirm(`Do you really want to delete '${name}'?`)) {
-      personServices.deletePerson(id).then((response) => {
-        alert(`${name} was deleted.`);
-        setPersonsToShow(personsToShow.filter((person) => person.id !== id));
-      });
+      personServices
+        .deletePerson(id)
+        .then((response) => {
+          setPersons(persons.filter((person) => person.id !== id));
+          setPersonsToShow(personsToShow.filter((person) => person.id !== id));
+          setSuccessMessage(`Deleted ${name}`);
+          setTimeout(() => {
+            setSuccessMessage(null);
+          }, 5000);
+        })
+        .catch((error) => {
+          setErrorMessage("An error occured");
+          setTimeout(() => {
+            setErrorMessage(null);
+          }, 5000);
+        });
     }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
-
+      <ErrorNotification message={errorMessage} />
+      <SuccessNotification message={successMessage} />
       <EntryForm
         savePerson={savePerson}
         newName={newName}
@@ -119,6 +169,7 @@ const App = () => {
         handleNameChange={handleNameChange}
         handleNumberChange={handleNumberChange}
       />
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <PersonsList personsToShow={personsToShow} handleDelete={handleDelete} />
     </div>
   );
