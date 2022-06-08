d16d2dc53d0ac956630ea9dc91b1126d30fcdc57
Aaron Donaldson <56371059+atreids@users.noreply.github.com>
2022-06-08 15:39:37 +0100

    finished exercises 2.19-2.20, part 2 complete

--- part2/exercises2-phonebook/src/components/EntryForm.js
+++ part2/exercises2-phonebook/src/components/EntryForm.js
@@ -3,17 +3,11 @@ import React from "react";
 const EntryForm = (props) => {
   return (
     <form onSubmit={props.savePerson}>
-      <div>
-        name:
-        <input value={props.newName} onChange={props.handleNameChange} />
-      </div>
-      <div>
-        number:
-        <input value={props.newNumber} onChange={props.handleNumberChange} />
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
+      <label>name</label>
+      <input value={props.newName} onChange={props.handleNameChange} />
+      <label>number</label>
+      <input value={props.newNumber} onChange={props.handleNumberChange} />
+      <button type="submit">add</button>
     </form>
   );
 };
