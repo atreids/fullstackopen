200ae0995d7e342096acda70d6e53b59e6e6c684
Aaron Donaldson <atreids@outlook.com>
2022-06-09 18:55:59 +0100

    finished example work up to exercise 3.7 in part 3a

--- part3/index.js
+++ part3/index.js
@@ -26,6 +26,15 @@ let notes = [
   },
 ];
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+app.use(requestLogger);
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
@@ -74,6 +83,11 @@ app.post("/api/notes", (request, response) => {
 
   response.json(note);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
