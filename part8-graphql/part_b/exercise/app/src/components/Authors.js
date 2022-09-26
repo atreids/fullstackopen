1d35073978ad50356970d69def9cb0645aa268ca
Aaron Donaldson <56371059+atreids@users.noreply.github.com>
2022-09-26 14:25:34 +0100

    exercise: graphql part_b exercise 8.8

--- part8-graphql/part_b/exercise/app/src/components/Authors.js
+++ part8-graphql/part_b/exercise/app/src/components/Authors.js
@@ -1,30 +1,42 @@
-const Authors = (props) => {
-  if (!props.show) {
-    return null
-  }
-  const authors = []
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries'
 
-  return (
-    <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.name}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+const Authors = (props) => {
+    const result = useQuery(ALL_AUTHORS)
+
+    if (!props.show) {
+        return null
+    }
+
+    console.log(result)
+
+    if (result.loading) {
+        return <div>loading...</div>
+    }
+
+    const authors = result.data.allAuthors
+
+    return (
+        <div>
+            <h2>authors</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th>born</th>
+                        <th>books</th>
+                    </tr>
+                    {authors.map((a) => (
+                        <tr key={a.name}>
+                            <td>{a.name}</td>
+                            <td>{a.born}</td>
+                            <td>{a.bookCount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Authors
