6cb1cf76f6b15d315fa6ceb408b8e26ca465dee3
Aaron Donaldson <56371059+atreids@users.noreply.github.com>
2022-09-26 12:02:02 +0100

    tutorial: graphql part_b client apollo-client/making-queries

--- part8-graphql/part_b/tutorial/app/src/app.js
+++ part8-graphql/part_b/tutorial/app/src/app.js
@@ -1,10 +1,36 @@
+import { gql, useQuery } from '@apollo/client'
 
-function App() {
-  return (
-    <div>
-      <p>Hello Aaron</p>
-    </div>
-  );
+const ALL_PERSONS = gql`
+    query {
+        allPersons {
+            name
+            phone
+            id
+        }
+    }
+`
+
+const Persons = ({ persons }) => {
+    return (
+        <div>
+            <h2>Persons</h2>
+            {persons.map((p) => (
+                <div key={p.name}>
+                    {p.name} {p.phone}
+                </div>
+            ))}
+        </div>
+    )
 }
 
-export default App;
+const App = () => {
+    const result = useQuery(ALL_PERSONS)
+
+    if (result.loading) {
+        return <div>loading...</div>
+    }
+
+    return <Persons persons={result.data.allPersons} />
+}
+
+export default App
--- part8-graphql/part_b/tutorial/app/src/index.js
+++ part8-graphql/part_b/tutorial/app/src/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import {
+    ApolloClient,
+    HttpLink,
+    InMemoryCache,
+    gql,
+    ApolloProvider,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: 'http://localhost:4000',
+    }),
+})
+
+const query = gql`
+    query {
+        allPersons {
+            name
+            phone
+            address {
+                street
+                city
+            }
+            id
+        }
+    }
+`
+
+client.query({ query }).then((response) => {
+    console.log(response.data)
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+)
